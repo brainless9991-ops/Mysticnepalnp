17c45d2387f2f4f4675b92738e1a48dc6a366902
sharad Gajurel <brainless9991@gmail.com>
2026-07-23 21:18:35 +0545

    Update API message

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
     console.log("========================================");
-    console.log(`🏠 API Home        : http://localhost:${PORT}/`);
+    console.log(`🏠 API Home        : http://localhost:${PORT}/frontend/index/index.html`);
     console.log(`🌐 Frontend        : http://localhost:${PORT}/frontend/index/index.html`);
     console.log(`🛠️ Admin Panel     : http://localhost:${PORT}/admin`);
     console.log(`🛠️ Admin Panel     : http://localhost:${PORT}/adminabout`);
