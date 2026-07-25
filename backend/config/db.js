844017fe11aa3e484b3638c6cc45fa7990d19624
sharad Gajurel <brainless9991@gmail.com>
2026-07-25 12:07:50 +0545

    Test update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2");
+/*const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
 
@@ -25,6 +25,32 @@ console.log("MySQL Connected");
 
 }
 
+});
+
+module.exports = connection;
+*/
+
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("TiDB Connected");
+  }
 });
 
 module.exports = connection;
